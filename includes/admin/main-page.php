2fc93e3f77f94b9795743a770b0b8fa7469de278
onetarek <onetarek@gmail.com>
2019-03-08 02:51:56 +0600

    Update main dashboard page design 2

--- includes/admin/main-page.php
+++ includes/admin/main-page.php
@@ -8,7 +8,16 @@ if( ! defined( 'ABSPATH' ) ) exit;
 	<br />
 	<div class="adg_section">
 
-	<a href="admin.php?page=adguru_zone"><div class="adg_feature_card item-1"><div class="feature_name">Zone</div></div></a>
+		<div class="adg_feature_card item-1">
+			<div class="feature_name">
+				<a href="admin.php?page=adguru_zone">Zone</a>
+			</div>
+			<div class="link-area">
+				<div class="card-icon-col col_2"><a class="card-link" href="admin.php?page=adguru_zone&manager_tab=all" title="All Zones"><span class="card-icon card-icon-all"></span><div class="card-link-text">All</div></a></div>
+				<div class="card-icon-col col_2 last-col"><a class="card-link" href="admin.php?page=adguru_zone&manager_tab=edit" title="Add new Zone"><span class="card-icon card-icon-new"><div class="card-link-text">New</div></a></div>
+				<div style="clear:left"></div>
+			</div>
+		</div>
 	<?php 
 	//ad types menus
 	$ad_types = adguru()->ad_types->types;
@@ -18,10 +27,24 @@ if( ! defined( 'ABSPATH' ) ) exit;
 	{ 
 		$i++;
 		$class_name = ($i % 4 == 0 )? 'adg_feature_card item-'.($i).' last-col' : 'adg_feature_card item-'.($i);
+		$ptype = ADGURU_ADMANAGER_PAGE_SLUG_PREFIX.$type;
 	?>
-		<a href="admin.php?page=<?php echo ADGURU_ADMANAGER_PAGE_SLUG_PREFIX.$type ?>"><div class="<?php echo $class_name;?>"><div class="feature_name"><?php echo $args['plural_name'] ?></div></div></a>
+		
+		<div class="<?php echo $class_name;?>">
+			<div class="feature_name">
+				<a href="admin.php?page=<?php echo $ptype ?>"><?php echo $args['plural_name'] ?></a>
+			</div>
+			<div class="link-area">
+				<div class="card-icon-col col_3"><a class="card-link" href="admin.php?page=<?php echo $ptype ?>&manager_tab=all"><span class="card-icon card-icon-all"></span><div class="card-link-text">All</div></a></div>
+				<div class="card-icon-col col_3"><a class="card-link" href="admin.php?page=<?php echo $ptype ?>&manager_tab=edit"><span class="card-icon card-icon-new"></span><div class="card-link-text">New</div></a></div>
+				<div class="card-icon-col col_3 last-col"><a class="card-link" href="admin.php?page=<?php echo $ptype ?>&manager_tab=links"><span class="card-icon card-icon-link"></span><div class="card-link-text">Links</div></a></div>
+				<div style="clear:left"></div>
+			</div>
+		</div>
+	
 	<?php 
 	}
+
 	?>
 		<div style="clear:both"></div>
 	</div>
@@ -41,6 +64,7 @@ if( ! defined( 'ABSPATH' ) ) exit;
 			box-sizing: border-box;
 			border: 0px solid #eeeeee;
 			border-radius: 4px;
+			border-bottom: 5px solid #eeeeee;
 			margin-right:2.6666667%;
 			margin-bottom: 15px;
 			background: #95AFC0;
@@ -49,38 +73,121 @@ if( ! defined( 'ABSPATH' ) ) exit;
 			-moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
 			box-shadow: 0 1px 2px rgba(0,0,0,.2);
 			float: left;
-			padding: 70px 0;
 			text-align: center;
 			text-transform: uppercase;
 		}
 		.adg_feature_card.last-col{
 			margin-right: 0%;
-
 		}
-		.adg_section .adg_feature_card:hover{
-			background: #BADC58;
-		}
+		
+		.adg_feature_card.item-1{ background: #6AB04C; border-bottom-color:#6AB04C; }
+		.adg_feature_card.item-2{ background: #EB4C4B; border-bottom-color:#EB4C4B; }
+		.adg_feature_card.item-3{ background: #F0932B; border-bottom-color:#F0932B; }
+		.adg_feature_card.item-4{ background: #F9CA23; border-bottom-color:#F9CA23; /*#E3B71C;*/ }
+		
+		.adg_feature_card.item-1:hover{ background: #7AB439; }
+		.adg_feature_card.item-2:hover{ background: #FF7979; }
+		.adg_feature_card.item-3:hover{ background: #FEBE76; }
+		.adg_feature_card.item-4:hover{ background: #F6E58D; /*#F9CA23;*/ }
+		
 		.adg_feature_card .feature_name{
-
 			font-size: 25px;
 			line-height: 30px;
 			text-align: center;
 			color: #ffffff;
+			margin-top: 40px;
+			margin-bottom: 40px;
 		}
-		.item-1{ background: #6AB04C; }
-		.item-2{ background: #EB4C4B; }
-		.item-3{ background: #F0932B; }
-		.item-4{ background: #F9CA23; }
-		.item-5{ background: #BADC58; }
-		.item-6{ background: #FF7979; }
-		.item-7{ background: #FEBE76; }
-		.item-8{ background: #F6E58D; }
+		.adg_feature_card .feature_name a{
+			color: #ffffff;
+			text-decoration: none;
+		}
+		.adg_feature_card .link-area{
+			background: #BADC58;
+			margin-top: 10px;
+			padding: 0px;
+			width: 100%;
+		}
+		.card-icon-col{
+			box-sizing: border-box;
+			border-right: 1px solid #d2ed82;
+			float: left;
+		}
+		.card-icon-col.col_2{
+			width: 50%;
+		}
+		.card-icon-col.col_3{
+			width: 33.3333%;
+
+		}
+		.card-icon-col.last-col{
+			border-right: 0px;
+		}
+		.card-link{
+			width: 100%;
+			padding: 18px 0px 3px 0px;
+			text-decoration: none;
+			display: block;
+			color: #ffffff;
+			text-align: center;
+			font-size: 10px;
+		}
+		.card-link-text{
+			font-size: 10px;
+			line-height: 10px;
+			display: block;
+			visibility: hidden;
+			margin-top: -5px;
+		}
+		.card-link:hover .card-link-text{
+			visibility: visible;
+		}
+		.adg_feature_card .card-icon:before{
+			font-family: "dashicons";
+		 	content: "\f103";
+		 	color: #ffffff;
+			display: inline-block;
+			-webkit-font-smoothing: antialiased;
+			font-weight: normal;
+			vertical-align: top;
+			font-size: 30px;
+			height: 30px;
+			
+		}
 		
-		.adg_feature_card.item-1:hover{ background: #BADC58; }
-		.adg_feature_card.item-2:hover{ background: #FF7979; }
-		.adg_feature_card.item-3:hover{ background: #FEBE76; }
-		.adg_feature_card.item-4:hover{ background: #F6E58D; }
+
+		.adg_feature_card .card-icon.card-icon-all:before{
+			content: "\f163";
+		}
+		.adg_feature_card .card-icon.card-icon-new:before{
+			content: "\f502";
+		}
+		.adg_feature_card .card-icon.card-icon-link:before{
+			content: "\f103";
+		}
 		
+
+
+		.item-1 .card-link:hover, .item-1 .card-link:hover .card-icon:before{
+			color: #6AB04C;
+		}
+
+		.item-2 .card-link:hover, .item-2 .card-link:hover .card-icon:before{
+			color: #EB4C4B;
+		}
+		.item-3 .card-link:hover, .item-3 .card-link:hover .card-icon:before{
+			color: #F0932B;
+		}
+		.item-4 .card-link:hover, .item-4 .card-link:hover .card-icon:before{
+			color: #E3B71C;
+		}
+
+		
+
+
+
+
+
 
 		#user_guide_box .ui-accordion-header.ui-state-default{
 			background: #95AFC0;

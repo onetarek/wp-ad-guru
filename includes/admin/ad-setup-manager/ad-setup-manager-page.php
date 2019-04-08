0136c3f768b35195e362ae2bb6d2a28b2c5ca133
onetarek <onetarek@gmail.com>
2019-04-08 11:59:06 +0600

    Ad Setup Manger : developing UI design

--- includes/admin/ad-setup-manager/ad-setup-manager-page.php
+++ includes/admin/ad-setup-manager/ad-setup-manager-page.php
@@ -75,7 +75,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 			<div class="condition-set">
 				<div class="set-header">
 					<span class="ec-btn"></span>
-					Single &gt; Post
+					Archive &raquo; Tag &raquo; <input type="text" size="12" placeholder="Tag Name" />
 					<div class="cs-box">
 
 						<select class="country-select" style="width: 321px;"><option value="--">---Select A Country---</option><option value="US">United States</option><option value="GB">United Kingdom</option><option value="CA">Canada</option><option value="AU">Australia</option><option value="NZ">New Zealand</option><optgroup label="- - - - - - - - - - - - - - - - - - - - - - - -"></optgroup><option value="AF">Afghanistan</option><option value="AX">Aland Islands</option><option value="AL">Albania</option><option value="DZ">Algeria</option><option value="AS">American Samoa</option><option value="AD">Andorra</option><option value="AO">Angola</option><option value="AI">Anguilla</option><option value="A1">Anonymous Proxy</option><option value="AQ">Antarctica</option><option value="AG">Antigua and Barbuda</option><option value="AR">Argentina</option><option value="AM">Armenia</option><option value="AW">Aruba</option><option value="AP">Asia/Pacific Region</option><option value="AT">Austria</option><option value="AZ">Azerbaijan</option><option value="BS">Bahamas</option><option value="BH">Bahrain</option><option value="BD">Bangladesh</option><option value="BB">Barbados</option><option value="BY">Belarus</option><option value="BE">Belgium</option><option value="BZ">Belize</option><option value="BJ">Benin</option><option value="BM">Bermuda</option><option value="BT">Bhutan</option><option value="BO">Bolivia</option><option value="BQ">Bonaire, Saint Eustatius and Saba</option><option value="BA">Bosnia and Herzegovina</option><option value="BW">Botswana</option><option value="BV">Bouvet Island</option><option value="BR">Brazil</option><option value="IO">British Indian Ocean Territory</option><option value="BN">Brunei Darussalam</option><option value="BG">Bulgaria</option><option value="BF">Burkina Faso</option><option value="BI">Burundi</option><option value="KH">Cambodia</option><option value="CM">Cameroon</option><option value="CV">Cape Verde</option><option value="KY">Cayman Islands</option><option value="CF">Central African Republic</option><option value="TD">Chad</option><option value="CL">Chile</option><option value="CN">China</option><option value="CX">Christmas Island</option><option value="CC">Cocos (Keeling) Islands</option><option value="CO">Colombia</option><option value="KM">Comoros</option><option value="CG">Congo</option><option value="CD">Congo, The Democratic Republic of the</option><option value="CK">Cook Islands</option><option value="CR">Costa Rica</option><option value="CI">Cote D'Ivoire</option><option value="HR">Croatia</option><option value="CU">Cuba</option><option value="CW">Curacao</option><option value="CY">Cyprus</option><option value="CZ">Czech Republic</option><option value="DK">Denmark</option><option value="DJ">Djibouti</option><option value="DM">Dominica</option><option value="DO">Dominican Republic</option><option value="EC">Ecuador</option><option value="EG">Egypt</option><option value="SV">El Salvador</option><option value="GQ">Equatorial Guinea</option><option value="ER">Eritrea</option><option value="EE">Estonia</option><option value="ET">Ethiopia</option><option value="EU">Europe</option><option value="FK">Falkland Islands (Malvinas)</option><option value="FO">Faroe Islands</option><option value="FJ">Fiji</option><option value="FI">Finland</option><option value="FR">France</option><option value="GF">French Guiana</option><option value="PF">French Polynesia</option><option value="TF">French Southern Territories</option><option value="GA">Gabon</option><option value="GM">Gambia</option><option value="GE">Georgia</option><option value="DE">Germany</option><option value="GH">Ghana</option><option value="GI">Gibraltar</option><option value="GR">Greece</option><option value="GL">Greenland</option><option value="GD">Grenada</option><option value="GP">Guadeloupe</option><option value="GU">Guam</option><option value="GT">Guatemala</option><option value="GG">Guernsey</option><option value="GN">Guinea</option><option value="GW">Guinea-Bissau</option><option value="GY">Guyana</option><option value="HT">Haiti</option><option value="HM">Heard Island and McDonald Islands</option><option value="VA">Holy See (Vatican City State)</option><option value="HN">Honduras</option><option value="HK">Hong Kong</option><option value="HU">Hungary</option><option value="IS">Iceland</option><option value="IN">India</option><option value="ID">Indonesia</option><option value="IR">Iran, Islamic Republic of</option><option value="IQ">Iraq</option><option value="IE">Ireland</option><option value="IM">Isle of Man</option><option value="IL">Israel</option><option value="IT">Italy</option><option value="JM">Jamaica</option><option value="JP">Japan</option><option value="JE">Jersey</option><option value="JO">Jordan</option><option value="KZ">Kazakhstan</option><option value="KE">Kenya</option><option value="KI">Kiribati</option><option value="KP">Korea, Democratic People's Republic of</option><option value="KR">Korea, Republic of</option><option value="KW">Kuwait</option><option value="KG">Kyrgyzstan</option><option value="LA">Lao People's Democratic Republic</option><option value="LV">Latvia</option><option value="LB">Lebanon</option><option value="LS">Lesotho</option><option value="LR">Liberia</option><option value="LY">Libya</option><option value="LI">Liechtenstein</option><option value="LT">Lithuania</option><option value="LU">Luxembourg</option><option value="MO">Macau</option><option value="MK">Macedonia</option><option value="MG">Madagascar</option><option value="MW">Malawi</option><option value="MY">Malaysia</option><option value="MV">Maldives</option><option value="ML">Mali</option><option value="MT">Malta</option><option value="MH">Marshall Islands</option><option value="MQ">Martinique</option><option value="MR">Mauritania</option><option value="MU">Mauritius</option><option value="YT">Mayotte</option><option value="MX">Mexico</option><option value="FM">Micronesia, Federated States of</option><option value="MD">Moldova, Republic of</option><option value="MC">Monaco</option><option value="MN">Mongolia</option><option value="ME">Montenegro</option><option value="MS">Montserrat</option><option value="MA">Morocco</option><option value="MZ">Mozambique</option><option value="MM">Myanmar</option><option value="NA">Namibia</option><option value="NR">Nauru</option><option value="NP">Nepal</option><option value="NL">Netherlands</option><option value="NC">New Caledonia</option><option value="NI">Nicaragua</option><option value="NE">Niger</option><option value="NG">Nigeria</option><option value="NU">Niue</option><option value="NF">Norfolk Island</option><option value="MP">Northern Mariana Islands</option><option value="NO">Norway</option><option value="OM">Oman</option><option value="O1">Other</option><option value="PK">Pakistan</option><option value="PW">Palau</option><option value="PS">Palestinian Territory</option><option value="PA">Panama</option><option value="PG">Papua New Guinea</option><option value="PY">Paraguay</option><option value="PE">Peru</option><option value="PH">Philippines</option><option value="PN">Pitcairn Islands</option><option value="PL">Poland</option><option value="PT">Portugal</option><option value="PR">Puerto Rico</option><option value="QA">Qatar</option><option value="RE">Reunion</option><option value="RO">Romania</option><option value="RU">Russian Federation</option><option value="RW">Rwanda</option><option value="BL">Saint Barthelemy</option><option value="SH">Saint Helena</option><option value="KN">Saint Kitts and Nevis</option><option value="LC">Saint Lucia</option><option value="MF">Saint Martin</option><option value="PM">Saint Pierre and Miquelon</option><option value="VC">Saint Vincent and the Grenadines</option><option value="WS">Samoa</option><option value="SM">San Marino</option><option value="ST">Sao Tome and Principe</option><option value="A2">Satellite Provider</option><option value="SA">Saudi Arabia</option><option value="SN">Senegal</option><option value="RS">Serbia</option><option value="SC">Seychelles</option><option value="SL">Sierra Leone</option><option value="SG">Singapore</option><option value="SX">Sint Maarten (Dutch part)</option><option value="SK">Slovakia</option><option value="SI">Slovenia</option><option value="SB">Solomon Islands</option><option value="SO">Somalia</option><option value="ZA">South Africa</option><option value="GS">South Georgia and the South Sandwich Islands</option><option value="SS">South Sudan</option><option value="ES">Spain</option><option value="LK">Sri Lanka</option><option value="SD">Sudan</option><option value="SR">Suriname</option><option value="SJ">Svalbard and Jan Mayen</option><option value="SZ">Swaziland</option><option value="SE">Sweden</option><option value="CH">Switzerland</option><option value="SY">Syrian Arab Republic</option><option value="TW">Taiwan</option><option value="TJ">Tajikistan</option><option value="TZ">Tanzania, United Republic of</option><option value="TH">Thailand</option><option value="TL">Timor-Leste</option><option value="TG">Togo</option><option value="TK">Tokelau</option><option value="TO">Tonga</option><option value="TT">Trinidad and Tobago</option><option value="TN">Tunisia</option><option value="TR">Turkey</option><option value="TM">Turkmenistan</option><option value="TC">Turks and Caicos Islands</option><option value="TV">Tuvalu</option><option value="UG">Uganda</option><option value="UA">Ukraine</option><option value="AE">United Arab Emirates</option><option value="UM">United States Minor Outlying Islands</option><option value="UY">Uruguay</option><option value="UZ">Uzbekistan</option><option value="VU">Vanuatu</option><option value="VE">Venezuela</option><option value="VN">Vietnam</option><option value="VG">Virgin Islands, British</option><option value="VI">Virgin Islands, U.S.</option><option value="WF">Wallis and Futuna</option><option value="EH">Western Sahara</option><option value="YE">Yemen</option><option value="ZM">Zambia</option><option value="ZW">Zimbabwe</option></select>
@@ -83,6 +83,47 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 					</div>
 					<div class="ac-box">
 						<span class="ac-btn"></span>
+					</div>
+					<div class="page-type-list-box">
+						<span class="title">Select type of page</span>
+						<ul class="page-type-list">
+							<li>Home</li>
+							<li>
+								<span class="group-name">Single Page</span>
+								<ul>
+									<li>Any type post</li>
+									<li>Post</li>
+									<li>Page</li>
+								</ul>
+							</li>
+							<li>
+								<span class="group-name">Taxonomy Archive page</span>
+								<ul>
+									<li>Any Taxonomy page</li>
+									<li>
+										<span class="group-name">Category</span>
+										<ul>
+											<li>Any Cagegory</li>
+											<li>Uncategorized</li>
+											<li>Tutorial</li>
+										</ul>
+									</li>
+									<li>
+										<span class="group-name">Tag</span>
+										<ul>
+											<li>Any Tag</li>
+											<li>
+												Specific Tag
+											</li>
+										</ul>
+									</li>
+									
+								</ul>
+							</li>
+							<li>Author Archive Page</li>
+							<li>Search Result Page</li>
+							<li>404 Page</li>
+						</ul>
 					</div>
 				</div>
 				<div class="set-body">
@@ -100,7 +141,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 									<div class="control-box">
 										<span class="percentage-box"><span class="percentage-label">Rotate</span><input type="number" class="percentage" value="100" max="100" min="0"> %</span>
 										<a class="edit-btn" href="#" target="_blank" title="Edit this ad"></a>
-										<span class="close-btn" title="Remove this ad"></span>
+										<span class="remove-btn" title="Remove this ad"></span>
 									</div>
 								</div><!-- /.ad -->
 								<div class="ad">
@@ -108,7 +149,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 									<div class="control-box">
 										<span class="percentage-box"><span class="percentage-label">Rotate</span><input type="number" class="percentage" value="100" max="100" min="0"> %</span>
 										<a class="edit-btn" href="#" target="_blank" title="Edit this ad"></a>
-										<span class="close-btn" title="Remove this ad"></span>
+										<span class="remove-btn" title="Remove this ad"></span>
 									</div>
 								</div><!-- /.ad -->
 								<div class="ad">
@@ -116,7 +157,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 									<div class="control-box">
 										<span class="percentage-box"><span class="percentage-label">Rotate</span><input type="number" class="percentage" value="100" max="100" min="0"> %</span>
 										<a class="edit-btn" href="#" target="_blank" title="Edit this ad"></a>
-										<span class="close-btn" title="Remove this ad"></span>
+										<span class="remove-btn" title="Remove this ad"></span>
 									</div>
 									<div class="more">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
 								</div><!-- /.ad -->
@@ -216,6 +257,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 		margin-bottom: 10px;
 		overflow: hidden;
 		background: #ffffff;
+		position: relative;
 
 	}
 	.condition-set .set-header{
@@ -484,7 +526,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 		color: #000000;
 	}
 
-	.condition-set .ad .control-box .close-btn{
+	.condition-set .ad .control-box .remove-btn{
 		width: 30px;
 		height: 30px;
 		display: inline-block;
@@ -493,7 +535,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 		cursor: pointer;
 		color: #bbbbbb;
 	}
-	.condition-set .ad .control-box .close-btn::before{
+	.condition-set .ad .control-box .remove-btn::before{
 		font-family: "dashicons";
 	 	content: "\f335";
 		display: inline-block;
@@ -503,7 +545,7 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 		font-size: 22px;
 		line-height: 30px;
 	}
-	.condition-set .ad:hover .control-box  .close-btn{
+	.condition-set .ad:hover .control-box  .remove-btn{
 		color: #ff0000;
 	}
 	.condition-set .add-ad-btn-box{
@@ -663,5 +705,39 @@ if( $use_zone ){ $editor_title = sprintf( __("Setup %s to Zone", "adguru" ) , $c
 	#add_condition_set_btn:hover{
 		background: #ffffff;
 	}
-	
+
+
+
+	.condition-set .set-header .page-type-list-box{
+		width:300px;
+		height: 400px;
+		overflow: scroll;
+		position: absolute;
+		background: #f5f5f5;
+		top:40px;
+		left: 3px;
+		border: 1px solid #cccccc;
+		border-top: none;
+		z-index: 10;
+	}
+	.page-type-list-box .title{
+		text-align: center;
+		display: block;
+		font-size: 15px;
+		font-weight: normal;
+		color: #000000;
+		border-bottom: 1px dashed #cccccc;
+		padding: 5px;
+
+	}
+	.page-type-list-box .page-type-list{
+		padding: 10px;
+		margin: 0px;
+	}
+	.page-type-list .group-name{
+		font-weight: bold;
+	}
+	.page-type-list ul{
+		padding-left: 20px;
+	}
 </style>

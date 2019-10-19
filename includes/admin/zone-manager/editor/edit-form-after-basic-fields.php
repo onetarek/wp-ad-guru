9e42179024cbbaeb190374bae5f3f7bc11d6fdc9
onetarek <onetarek@gmail.com>
2019-10-20 04:05:34 +0600

    Zone Editor : general form : remove heading and border top

--- includes/admin/zone-manager/editor/edit-form-after-basic-fields.php
+++ includes/admin/zone-manager/editor/edit-form-after-basic-fields.php
@@ -1,5 +1,4 @@
-<div id="zone_editor_general_box" class="postbox">
-	<h2 class='hndle'><span><?php _e('General', 'adguru')?></span></h2>
+<div id="zone_editor_general_box" class="postbox" style="border-top:0;">
 	<div class="inside">
 		<?php adguru_show_zone_general_form( $zone ); ?>
 	</div><!-- ./inside -->

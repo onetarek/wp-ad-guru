843992c65f281466d2a6f8c7e06b4085442540ef
onetarek <onetarek@gmail.com>
2019-10-29 03:53:55 +0600

    Zone Editor : update page type fields

--- includes/admin/zone-manager/editor/form-general.php
+++ includes/admin/zone-manager/editor/form-general.php
@@ -1,4 +1,5 @@
 <?php
+$general_form_page_type_detail = adguru_get_page_types_multicheck_options_detail();
 
 $general_form_args = array(
 	'id' => 'zone_general_form',
@@ -26,30 +27,43 @@ $general_form_args = array(
 			),
 		),
 
-		'general_page_types' => array(
-			'type' 	=> 'multicheck',
-			'id'	=> 'general_page_types',
-			'label'	=> __("Page types", 'adguru' ),
-			'items_direction' => 'horizontal',
-			'on_off_values' => array( "1", "0" ),
-			'default'	=> array(
-				'any_page' => 1,
-				'singel_post' => 0,
-				'taxonomy_archive' => 0,
-				'date_archive' => 0,
-				'search_result' => 0,
-				'author_archive' => 0
-			),
-			'options' => array(
-				'any_page' => __("Any type page", 'adguru' ),
-				'singel_post' => __("Single Post", 'adguru' ),
-				'taxonomy_archive' => __("Taxonomy archive", 'adguru' ),
-				'date_archive' => __("Date archive", 'adguru' ),
-				'search_result' => __("Search result", 'adguru' ),
-				'author_archive' => __("Author archive", 'adguru' )
-				
-			),
-		),
+		'general_page_types_group' => array(
+			'type'	=> 'group',
+			'group_type' => 'vertical',
+			'id' => 'general_page_types_group',
+			'label' => __("Page Types", 'adguru' ),
+			'help' => __("Select type of pages where you want to insert this zone", 'adguru' ),
+			'fields' => array(
+				'general_page_types_misc' => array(
+					'type' 	=> 'multicheck',
+					'id'	=> 'general_page_types_misc',
+					'label'	=> '',
+					'items_direction' => 'vertical',
+					'on_off_values' => array( "1", "0" ),
+					'default'	=> $general_form_page_type_detail['misc_type_page_defaults'],
+					'options' => $general_form_page_type_detail['misc_type_page_options'],
+				),
+
+				'general_page_types_single' => array(
+					'type' 	=> 'multicheck',
+					'id'	=> 'general_page_types_single',
+					'label'	=> sprintf('<strong>%s</strong>', __("Singe pages", 'adguru' ) ),
+					'items_direction' => 'horizontal',
+					'on_off_values' => array( "1", "0" ),
+					'default'	=> $general_form_page_type_detail['single_type_page_defaults'],
+					'options' => $general_form_page_type_detail['single_type_page_options'],
+				),
+				'general_page_types_archive' => array(
+					'type' 	=> 'multicheck',
+					'id'	=> 'general_page_types_archive',
+					'label'	=> sprintf('<strong>%s</strong>', __("Archive/List type pages", 'adguru' ) ),
+					'items_direction' => 'horizontal',
+					'on_off_values' => array( "1", "0" ),
+					'default'	=> $general_form_page_type_detail['archive_type_page_defaults'],
+					'options' => $general_form_page_type_detail['archive_type_page_options'],
+				),
+			)
+		),//end of sizing_custom_width_group
 
 		
 
@@ -96,7 +110,7 @@ function adguru_show_zone_general_form( $zone )
 			}
 
 		}
-write_log($general_data);
+	//write_log($general_data);
 		$general_form->set_data( $general_data );
 		//Before render modify the fields settings, specially update fields hidden status based on the value.
 		do_action('adguru_editor_form_modal_popup_animation_before_render', $general_form );
@@ -107,5 +121,63 @@ write_log($general_data);
 }
 
 
+
+function adguru_get_page_types_multicheck_options_detail(){
+	$detail = array();
+	$post_types = ADGURU_Helper::get_post_type_list();
+	$taxonomy_list = ADGURU_Helper::get_taxonomy_list();
+
+	$misc_type_page_options = array();
+	$misc_type_page_defaults = array();
+
+	$misc_type_page_options['home'] = __('Home page', 'adguru');
+	$misc_type_page_defaults['home'] = 1;
+	$misc_type_page_options['404_not_found'] = __('404 page', 'adguru');
+	$misc_type_page_defaults['404_not_found'] = 0;
+
+
+	$single_type_page_options = array();
+	$single_type_page_defaults = array();
+
+	$single_type_page_options['single_any'] = __('Any single page', 'adguru');
+	$single_type_page_defaults['single_any'] = 1;
+	foreach( $post_types as $type => $name )
+	{
+		$single_type_page_options[ 'single_'.$type ] = $name;
+		$single_type_page_defaults[ 'single_'.$type ] = 0;
+	}
+
+
+
+	$archive_type_page_options = array();
+	$archive_type_page_defaults = array();
+	$archive_type_page_options['archive_any'] =  __('Any archive page', 'adguru');
+	$archive_type_page_defaults['archive_any'] = 1;
+	foreach( $taxonomy_list as $key => $tax )
+	{
+		$archive_type_page_options['archive_'.$key] = $tax->label;
+		$archive_type_page_defaults['archive_'.$key] = 0;
+	}
+
+	$archive_type_page_options['archive_author'] =  __('Author', 'adguru');
+	$archive_type_page_defaults['archive_author'] = 0;
+
+	$archive_type_page_options['archive_search'] =  __('Search', 'adguru');
+	$archive_type_page_defaults['archive_search'] = 0;
+
+	$archive_type_page_options['archive_date'] =  __('Year/Month/Date', 'adguru');
+	$archive_type_page_defaults['archive_date'] = 0;
+
+	$detail['misc_type_page_options'] = $misc_type_page_options;
+	$detail['misc_type_page_defaults'] = $misc_type_page_defaults;
+	$detail['single_type_page_options'] = $single_type_page_options;
+	$detail['single_type_page_defaults'] = $single_type_page_defaults;
+	$detail['archive_type_page_options'] = $archive_type_page_options;
+	$detail['archive_type_page_defaults'] = $archive_type_page_defaults;
+	
+	//write_log($taxonomy_list, $post_types);
+	return $detail;
+
+}
 
 

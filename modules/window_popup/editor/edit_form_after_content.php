19e0a64fb4cc240cafcfe78ec074074287b44dbb
onetarek <onetarek@gmail.com>
2025-12-03 20:23:22 +0600

    Fix WordPress.Security.EscapeOutput.UnsafePrintingFunction

--- modules/window_popup/editor/edit_form_after_content.php
+++ modules/window_popup/editor/edit_form_after_content.php
@@ -1,20 +1,20 @@
 
 <div id="window_popup_ad_editor_sizing_box" class="postbox">
-	<h2 class='hndle'><span><?php _e('Sizing', 'wp-ad-guru')?></span></h2>
+	<h2 class='hndle'><span><?php esc_html_e('Sizing', 'wp-ad-guru')?></span></h2>
 	<div class="inside">
 		<?php adguru_show_window_popup_sizing_form( $ad ); ?>
 	</div><!-- ./inside -->
 </div><!-- /.postbox -->
 
 <div id="window_popup_ad_editor_popup_option_box" class="postbox">
-	<h2 class='hndle'><span><?php _e('Popup Option', 'wp-ad-guru')?></span></h2>
+	<h2 class='hndle'><span><?php esc_html_e('Popup Option', 'wp-ad-guru')?></span></h2>
 	<div class="inside">
 		<?php adguru_show_window_popup_popup_options_form( $ad ); ?>
 	</div><!-- ./inside -->
 </div><!-- /.postbox -->
 
 <div id="window_popup_ad_editor_triggering_box" class="postbox">
-	<h2 class='hndle'><span><?php _e('Triggering', 'wp-ad-guru')?></span></h2>
+	<h2 class='hndle'><span><?php esc_html_e('Triggering', 'wp-ad-guru')?></span></h2>
 	<div class="inside">
 		<?php adguru_show_window_popup_triggering_form( $ad ); ?>
 	</div><!-- ./inside -->
